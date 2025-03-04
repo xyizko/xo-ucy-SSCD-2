4c2f0037ef8291e8e206b6aad7d1ef47827b0041
xyizko <xyizko@proton.me>
2025-03-04 23:14:21 +0530

    - Lesson8 MD

--- SSCD/l8.sol
+++ SSCD/l8.sol
@@ -3,3 +3,30 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18 ;
+
+contract SimpleStorage {
+
+    uint256 myfavNum;
+
+    // Array List 
+    //uint256[] listOfFavNum; // [6,9,69];
+
+    // Custom type
+    struct Killa{
+        uint256 number;
+        string name;
+    }
+
+    function store(uint256 _favNum) public {
+        myfavNum = _favNum;
+    }
+
+    //view, pure 
+
+    function retrieve() public view returns (uint256){
+        return  myfavNum ;
+    }
+
+    // Defining the custom type 
+    Killa public kanz = Killa(69, "sword");
+}
