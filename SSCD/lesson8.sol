3f5b303e809929049e064e41c25df59245d9ea62
xyizko <xyizko@proton.me>
2025-03-04 22:22:45 +0530

    continue lesson

---
+++ SSCD/lesson8.sol
@@ -0,0 +1,13 @@
+// This contract file is the excercise work for SSCD Lesson 7 
+// Each lesson will have its own folder with own solidity file 
+// WARNING ! - Experimental nonsensical code here
+
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18 ;
+
+contract SimpleStorage {
+    // Fav number gets initialized 
+    uint256 favNumber = 7;
+}
